11916721aba0549a415acb1d165cc8407a004d8e
Djam <gc.djames@gmail.com>
2017-12-22 21:36:18 +0100

    Correction of intall script

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 # This file will be automatically sourced at the installation of your plugin
 # Use only if you need to perform changes on the user system such as installing software
 jv_debug "Downloading websocketd"
+jv_pg_mm_www_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"/www
 if [[ "$platform" == "linux" ]]; then
 	# Start in new terminal to avoid blocking Jarvis              
 	wget "https://github.com/joewalnes/websocketd/releases/download/v0.2.12/websocketd-0.2.12-linux_arm.zip"
